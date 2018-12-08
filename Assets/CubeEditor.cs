4566286b2a74cf5a019c01bf559ff2cc10dabc13
Tsukiyomi <>
2018-12-08 10:48:48 -0800

    Initial commit.

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
+    [Range(1f,20f)]
+    [SerializeField]
+    float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    void Start(){
+        textMesh = gameObject.GetComponentInChildren<TextMesh>();
+    }
+
+    void Update () {
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+	}
+}
