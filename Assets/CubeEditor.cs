d1284592a3254f3d1df0dd719abd8234a23f0e8c
Tsukiyomi <>
2018-12-09 09:47:31 -0800

    132: Explore Thy Neighbour

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -23,7 +23,7 @@ public class CubeEditor : MonoBehaviour {
     void SnapToGrid()
     {
         int gridSize = waypoint.GetGridSize();
-        transform.position = new Vector3(waypoint.GetGridPos().x, 0f, waypoint.GetGridPos().y);
+        transform.position = new Vector3(waypoint.GetGridPos().x * gridSize, 0f, waypoint.GetGridPos().y * gridSize);
     }
 
     void UpdateLabel()
@@ -31,7 +31,7 @@ public class CubeEditor : MonoBehaviour {
         TextMesh textMesh = gameObject.GetComponentInChildren<TextMesh>();
 
         int gridSize = waypoint.GetGridSize();
-        string labelText = waypoint.GetGridPos().x / gridSize + "," + waypoint.GetGridPos().y / gridSize;
+        string labelText = waypoint.GetGridPos().x + "," + waypoint.GetGridPos().y;
 
         textMesh.text = labelText;
         gameObject.name = labelText;
--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -8,10 +8,33 @@ public class Pathfinder : MonoBehaviour {
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
 
+    Vector2Int[] directions = {
+        Vector2Int.right,
+        Vector2Int.up,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
 	// Use this for initialization
 	void Start () {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            print("Exploring " + explorationCoordinates);
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            } catch {
+                //nada
+            }
+        }
     }
 
     private void ColorStartAndEnd()
