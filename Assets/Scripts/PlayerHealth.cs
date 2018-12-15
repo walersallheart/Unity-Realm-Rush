3e86b301d519a3ed2b3194ecdbd1829db1d5247e
Tsukiyomi <>
2018-12-15 08:50:29 -0800

    157 Health For Base
    Also complete level rebuild.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        health -= healthDecrease;
+    }
+}
